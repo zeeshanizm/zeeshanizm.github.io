0ba68a3b062d345c50bb28736cdf81183d8be392
Zeeshan Ahmad <162763821+zeeshanizm@users.noreply.github.com>
2026-02-19 15:39:58 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const roles=['Software Engineer','Automation Specialist','Full Stack Developer'];
-let i=0,j=0;
-const el=document.getElementById('typing');
-function type(){
- if(j<roles[i].length){el.textContent+=roles[i][j];j++;setTimeout(type,80);}
- else{setTimeout(()=>{el.textContent='';j=0;i=(i+1)%roles.length;type();},1500);}
-}
-type();
